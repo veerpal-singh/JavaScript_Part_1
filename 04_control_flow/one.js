9107e85246ac68b3bb549e96839ad219e84cbdfc
veerpal-singh <veer.veeru68@gmail.com>
2024-09-03 08:46:14 +0100

    add new file

---
+++ 04_control_flow/one.js
@@ -0,0 +1,40 @@
+// if
+
+const temperature = 41
+
+if(temperature === 40){
+    console.log("less then 50");    
+}else{
+    console.log("temperature is greater than 50");    
+}
+
+
+// <, >, >=, ==, !=, ===
+
+
+// const balance = 1000
+
+// if(balance < 500){
+//     console.log("Less than 500");    
+// }else if(balance < 750){
+//     console.log("Less than 750");    
+// }else if(balance < 900){
+//     console.log("less than 900");    
+// }else{
+//     console.log("less than 1200");    
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitCard){
+    console.log("Allow to buy course");    
+}
+
+if(loggedInFromEmail || loggedInFromGoogle){
+    console.log("user logged in");    
+}
+
