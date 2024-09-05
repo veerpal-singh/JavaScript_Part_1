0539d35ca1373e1a828fa186316d8b153f4405d2
veerpal-singh <veer.veeru68@gmail.com>
2024-09-05 18:34:49 +0100

    add new file

---
+++ 07_Events/one.js
@@ -0,0 +1,26 @@
+// Events => The change in the State of an object is known as an Event
+
+// . Mouse events (click, double click etc.)
+
+// . Keyboard events (keypress, keyup, keydown)
+
+// . Form events (submit etc.)
+
+// . Print event & many more
+
+
+// ************************************************************************
+
+
+// Event Object
+
+// It is a special object that has details about the event.
+
+// All event handlers have access to the Event Object's properties and methods.
+
+Node.evenet = (e) =>{
+
+    //handle.here
+}
+
+// e.target, e.type, e.clientX, e.clientY
