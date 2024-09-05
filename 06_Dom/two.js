d4cce8b52ca6aa89433af0cdfd8bab40db41519c
veerpal-singh <veer.veeru68@gmail.com>
2024-09-05 12:21:05 +0100

    add new file

---
+++ 06_Dom/two.js
@@ -0,0 +1,25 @@
+// DOM Attributes
+
+// getAttribute(attr) to get attribute value
+
+// setAttribute(attr, value) to set the attribute val th
+
+let div = document.querySelector("div")
+
+let id = div.getAttribute("id") // attribute name
+
+let change = div.setAttribute("id", "box")
+
+
+// ******************************** Style ********************************
+
+
+let divs = document.querySelector("div")
+
+div.style.backgroundColor = "green"
+
+div.style.fontSize = "24px"
+
+div.style.visibility = "hidden"
+
+
