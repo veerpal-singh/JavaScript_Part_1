08d03db58e53077e77b8d1af18683de3b9167779
veerpal-singh <veer.veeru68@gmail.com>
2024-09-07 09:57:01 +0100

    add new file

---
+++ 10_advance/02_Fatch.js
@@ -0,0 +1,17 @@
+async function getAllUsers() {
+
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users")        
+        
+
+        const data = await response.json()
+
+        console.log(data);
+
+    } catch (error) {
+        console.log("E:",error);
+        
+    }
+}
+
+getAllUsers()
