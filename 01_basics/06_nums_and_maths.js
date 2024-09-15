65bf654dd0b31d65dd5b82d9e21ebc66f69bf3de
veerpal-singh <veer.veeru68@gmail.com>
2024-09-15 05:09:16 +0100

    add new file

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -18,10 +18,17 @@ console.log(hundreds.toLocaleString("en-IN"));
 // ******************************* Maths **********************************
 
 console.log(Math);
+
 console.log(Math.abs(-4));
+
+console.log(Math.round(4.4)); // most use
+
 console.log(Math.ceil(4.2)); // 5
+
 console.log(Math.floor(4.9)); // 4
+
 console.log(Math.min(4,3,6,8));
+
 console.log(Math.max(4,3,6,8));
 
 console.log(Math.random());
