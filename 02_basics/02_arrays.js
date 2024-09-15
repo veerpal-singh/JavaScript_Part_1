7958ab9142ad1a1f6419f98e6fb85004f25564d1
veerpal-singh <veer.veeru68@gmail.com>
2024-09-15 05:50:46 +0100

    add new file

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,12 +1,32 @@
-const marvel_heros = ["thor", "Ironman", "spiderman"]
-const dc_heros = ["superman", "flash", "batman"]
+// const marvel_heros = ["thor", "Ironman", "spiderman"]
+// const dc_heros = ["superman", "flash", "batman"]
 
-// marvel_heros.push(dc_heros)
-// console.log(marvel_heros);
+// // marvel_heros.push(dc_heros)
+// // console.log(marvel_heros);
 
-// const allHeros = marvel_heros.concat(dc_heros)
-// console.log(allHeros);
+// // const allHeros = marvel_heros.concat(dc_heros)
+// // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros] // use this
-console.log(all_new_heros);
+// const all_new_heros = [...marvel_heros, ...dc_heros] // use this
+// console.log(all_new_heros);
+
+
+console.log(Array.isArray("Veer")); // false
+
+console.log(Array.from("Veer")); // change to array
+
+console.log(Array.from({name: "veer"})); // empty array // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3)); // change to array
+
+
+
+
+
+
+
 
