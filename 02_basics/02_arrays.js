d5d80ae6f49ae4a84144443d86f7eb1baa072b24
veerpal-singh <veer.veeru68@gmail.com>
2024-09-15 05:53:52 +0100

    add new file

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,14 +1,9 @@
-// const marvel_heros = ["thor", "Ironman", "spiderman"]
-// const dc_heros = ["superman", "flash", "batman"]
+const marvel_heros = ["thor", "Ironman", "spiderman"]
 
-// // marvel_heros.push(dc_heros)
-// // console.log(marvel_heros);
+const dc_heros = ["superman", "flash", "batman"]
 
-// // const allHeros = marvel_heros.concat(dc_heros)
-// // console.log(allHeros);
-
-// const all_new_heros = [...marvel_heros, ...dc_heros] // use this
-// console.log(all_new_heros);
+const all_new_heros = [...marvel_heros, ...dc_heros] // use this
+console.log(all_new_heros);
 
 
 console.log(Array.isArray("Veer")); // false
