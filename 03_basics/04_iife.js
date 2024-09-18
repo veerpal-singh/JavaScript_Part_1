a8b9113d310a563cbaba0a249c67c2affe1b6775
veerpal-singh <veer.veeru68@gmail.com>
2024-09-18 18:00:38 +0100

    add new file

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,9 +1,11 @@
 // Immediately Invoked Function Expressions (IIFE)
 
-(function chai (){  // Global Scope Poluction
+(function chai() {  // Global Scope Poluction
     console.log("DB CONNECTED");    // Named IIFe
 })();
 
-((name) =>{  // UnNamed IIFE (Arrow function)
-    console.log("DB CONNECTED");    
+((name) => {  // UnNamed IIFE (Arrow function)
+    console.log(`DB CONNECTED TWO ${name}`);
 })("veer")
+
+
