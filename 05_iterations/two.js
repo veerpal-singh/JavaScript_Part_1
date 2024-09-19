2be84dba52c8009452f729c64ed3251dd6f88b6d
veerpal-singh <veer.veeru68@gmail.com>
2024-09-19 08:25:29 +0100

    add new file

--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -1,36 +1,20 @@
 // For Array 
 
-// for of
+// for of loop
 
 const arr = [1,2,3,4,5,6]
 
 for (const num of arr) {
+
     console.log(num);    
 }
 
 const greetings = "Hello world"
 
 for (const greet of greetings) {
+
     console.log(`Each char is ${greet}`);    
 }
 
 
-// Map
 
-const map = new Map()  // Unique Value
-map.set('IN', "India")
-map.set('USA',"United States of America")
-map.set('Fr', "France")
-map.set('IN', "India")
-
-// console.log(map);
-
-// for(const key of map){     // Array
-//     console.log(key);
-    
-// }
-
-
-// for(const [key, value] of map){      // key, value
-//     console.log(key, ':-', value);    
-// }
