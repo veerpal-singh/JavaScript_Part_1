ea724523f0c9bd071d66760c01453dbe386533f1
veerpal-singh <veer.veeru68@gmail.com>
2024-09-21 06:47:25 +0100

    add new file

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -3,6 +3,17 @@
 const myNumbers = [1,2,3,4,5,6]
 
 const newNums = myNumbers.map((num) => {
-    return (num);    
+
+    // return num
+    // return num + 10;    
 })
 console.log(newNums);
+
+
+const newNums2 = myNumbers
+                .map((num) => num * 10) 
+                .map((num) => num + 1)
+                .filter((num) => num >= 40)
+
+
+console.log(newNums2);
