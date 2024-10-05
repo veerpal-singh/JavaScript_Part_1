f8ec1eaa29cdd44063294f5dbe57e185cdff77a6
veerpal-singh <veer.veeru68@gmail.com>
2024-10-05 05:26:40 +0100

    new changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -42,19 +42,44 @@ let str2 = " veer"
 let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+console.log("1" + 2); // 12
 
-// console.log( (3 + 4) * 5 % 3);
+console.log(1 + "2"); // 12
 
-console.log(+true);
-console.log(+"");
+console.log("1" + 2 + 2); // 122
+
+console.log(1 + 2 + "2"); // 32
+
+// *********************************************************************
+
+console.log(+true); // output => 1
+
+console.log(+""); // output => 0
 
 let gameCounter = 100
+
 ++gameCounter;
+
 console.log(gameCounter);
+
+
+let a = 13;
+
+// console.log("++a = ", ++a); output => 14
+
+console.log("a++ = ", a++); // output => 13
+
+console.log(a); // output => 14
+
+
+
+
+
+
+
+
+
+
 
 
 
