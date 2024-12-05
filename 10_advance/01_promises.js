cd99f6e188099c62f6918da3d6dad84076dc6b7e
veerpal-singh <veer.veeru68@gmail.com>
2024-12-05 07:01:21 +0000

    add new promise

--- 10_advance/01_promises.js
+++ 10_advance/01_promises.js
@@ -1,85 +1,22 @@
-// Promise => Object representing
-
 const promiseOne = new Promise((resolve, reject) => {
-
     setTimeout(() => {
-
-        console.log("Async task is complete");
-
-        resolve()
-
-    }, 1000)
-})
-
-promiseOne.then(() =>{
-    console.log("Promise consumed");
-
-})
-
-
-
-new Promise((resolve, reject) =>{
-
-    setTimeout(() =>{
-
-        console.log("Async task 2");
-
-        resolve()
-
-    }, 1000)
-
-}).then(() =>{
-    console.log("Async 2 resolved");
-
-})
-
-
-
-const promiseThree = new Promise((resolve, reject) =>{
-
-    setTimeout(() =>{
-
-        resolve({username: "chai", email: "chai@example.com"})
-
-    }, 1000)
-})
-
-promiseThree.then((user) =>{
-    console.log(user);
-
-})
-
-
-const promiseFour = new Promise((resolve, reject) => {
-
-    setTimeout(() => {
-
-        let error = false
-
+        let error = true;
         if (!error) {
-
-            resolve({ username: 'veer', password: "123" })
-
+            resolve({ username: "veer singh", password: '123' })
         } else {
-            reject("ERROR: SOMETHING WRONG")
+            reject("ERROR: js went wrong")
         }
-
     }, 1000)
 })
 
-promiseFour.then((user) => {
+const consumePromiseONe = async () => {
+    try {
+        const response = await promiseOne
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+        
+    }
 
-    console.log(user);
-
-    return user.username
-
-}).then((username) => {
-    console.log(username);
-
-}).catch((error) => {
-    console.log(error);
-
-}).finally(() => console.log("promises is either resolved or rejected"))
-
-
-
+}
+consumePromiseONe()
